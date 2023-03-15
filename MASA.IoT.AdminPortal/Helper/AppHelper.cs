bdd1f3787182fb1c3fe6f67834410354a897e334
dkx4m8w <External.Hao.Guo@vwfsag.com>
2023-03-15 22:14:59 +0800

    添加项目文件。

---
+++ MASA.IoT.AdminPortal/Helper/AppHelper.cs
@@ -0,0 +1,46 @@
+﻿namespace MASA.IoT.AdminPortal.Helper
+{
+    public class AppHelper
+    {
+        private static IConfiguration _config;
+
+        public AppHelper(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        /// <summary>
+        /// 读取指定节点的字符串-》返回配置的url
+        /// </summary>
+        /// <param name="sessions"></param>
+        /// <returns></returns>
+        public static string ReadAppSettings(params string[] sessions)
+        {
+            try
+            {
+                if (sessions.Any())
+                {
+                    return _config[string.Join(":", sessions)];
+                }
+            }
+            catch
+            {
+                return "";
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// 读取实体信息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        public static List<T> ReadAppSettings<T>(params string[] session)
+        {
+            List<T> list = new List<T>();
+            _config.Bind(string.Join(":", session), list);
+            return list;
+        }
+    }
+}
---
+++ MASA.IoT.AdminPortal/Helper/MqttHelper.cs
@@ -0,0 +1,56 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Formatter;
+
+namespace MASA.IoT.AdminPortal.Helper
+{
+    public class MqttHelper
+    {
+        private MqttFactory mqttFactory;
+        private IMqttClient mqttClient;
+        private MqttClientOptions mqttClientOptions;
+        private MqttClientSubscribeOptions mqttClientSubscribeOptions;
+        public MqttHelper(string mqttUrl, string clientID)
+        {
+            mqttFactory = new MqttFactory();
+            mqttClient = mqttFactory.CreateMqttClient();
+            mqttClientOptions = new MqttClientOptionsBuilder()
+                                  .WithTcpServer(mqttUrl, 2883)
+              .WithCredentials("emqx", "123456").WithProtocolVersion(MqttProtocolVersion.V500).Build();
+
+            mqttClientOptions.ClientId = clientID;
+        }
+
+
+        public async Task SendCmdAsync(string stringdata, string topicIndex)  //发布客户端的消息
+        {
+            if (!mqttClient.IsConnected)
+            {
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+            }
+            await mqttClient.PublishStringAsync($"samples/topic/cmd{topicIndex}", stringdata);
+        }
+        public async Task Connect_Client_Using_WebSockets(Func<MqttApplicationMessageReceivedEventArgs, Task> callback, string userName) //订阅客户端
+        {
+            /*
+             * This sample creates a simple MQTT client and connects to a public broker using a WebSocket connection.
+             * 
+             * This is a modified version of the sample _Connect_Client_! See other sample for more details.
+             */
+            mqttClientSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(f => { f.WithTopic($"v1/devices/{userName}/telemetry"); })
+                .Build();
+
+            var response = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+            Console.WriteLine("The MQTT client is connected.");
+
+            await Task.Delay(500);
+
+            mqttClient.ApplicationMessageReceivedAsync += callback;
+            var response2 = await mqttClient.SubscribeAsync(mqttClientSubscribeOptions, CancellationToken.None);
+
+
+        }
+    }
+}
